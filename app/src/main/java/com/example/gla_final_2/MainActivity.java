834139a39d22813a9b9db191ac3274d0ea0de324
Dushyantsisodia2k22 <sisodiadushyant2002@gmail.com>
2023-06-03 13:58:33 +0530

    login activity show only one time at the time of login

--- app/src/main/java/com/example/gla_final_2/MainActivity.java
+++ app/src/main/java/com/example/gla_final_2/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.gla_final_2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 
@@ -12,17 +13,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
-
-
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent iHome = new Intent(MainActivity.this, login_Activity.class);
-                startActivity(iHome);
-                finish();
+
+                SharedPreferences sharedPreferences = getSharedPreferences(login_Activity.PREFS_NAME, 0);
+
+                boolean hasloggedIn= sharedPreferences.getBoolean("hasLoggedIn",false);
+
+                if(hasloggedIn) {
+
+                    Intent iHome = new Intent(MainActivity.this, login_Activity.class);
+                    startActivity(iHome);
+                    finish();
+                }
+                else{
+                    Intent iHome = new Intent(MainActivity.this, login_Activity.class);
+                    startActivity(iHome);
+                    finish();
+                }
 
             }
         }, 4000);
--- app/src/main/java/com/example/gla_final_2/login_Activity.java
+++ app/src/main/java/com/example/gla_final_2/login_Activity.java
@@ -1,14 +1,14 @@
 package com.example.gla_final_2;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -19,9 +19,11 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.Objects;
 
-
-import java.util.Objects;
 public class login_Activity extends AppCompatActivity {
+
+    public static final String PREFS_NAME = "MyPrefsFile";
+    public static final String PREF_KEY_HAS_LOGGED = "hasLogged";
+
     EditText loginUsername, loginPassword;
     Button loginButton;
 
@@ -33,32 +35,38 @@ public class login_Activity extends AppCompatActivity {
         loginPassword = findViewById(R.id.password);
         loginButton = findViewById(R.id.loginbtn);
 
+        // Check if the user has already logged in
+        SharedPreferences sharedPreferences = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        boolean hasLogged = sharedPreferences.getBoolean(PREF_KEY_HAS_LOGGED, false);
+        if (hasLogged) {
+            startSelectionActivity();
+            return;
+        }
+
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(!validateUsername()| !validatePassword()){
-
-                }else{
-                    checkUser();
+                if (!validateUsername() || !validatePassword()) {
+                    return;
                 }
+                checkUser();
             }
         });
-
-
-
     }
+
     public Boolean validateUsername() {
-        String val = loginUsername.getText().toString();
+        String val = loginUsername.getText().toString().trim();
         if (val.isEmpty()) {
-            loginUsername.setError("cannot be empty");
+            loginUsername.setError("Username cannot be empty");
             return false;
         } else {
             loginUsername.setError(null);
             return true;
         }
     }
-    public Boolean validatePassword(){
-        String val = loginPassword.getText().toString();
+
+    public Boolean validatePassword() {
+        String val = loginPassword.getText().toString().trim();
         if (val.isEmpty()) {
             loginPassword.setError("Password cannot be empty");
             return false;
@@ -67,7 +75,8 @@ public class login_Activity extends AppCompatActivity {
             return true;
         }
     }
-    public void checkUser(){
+
+    public void checkUser() {
         String userUsername = loginUsername.getText().toString().trim();
         String userPassword = loginPassword.getText().toString().trim();
         DatabaseReference reference = FirebaseDatabase.getInstance().getReference("student");
@@ -75,25 +84,42 @@ public class login_Activity extends AppCompatActivity {
         checkUserDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if (snapshot.exists()){
+                if (snapshot.exists()) {
                     loginUsername.setError(null);
-                    String passwordFromDB = snapshot.child(userUsername).child("pass").getValue(String.class);
-                    if (Objects.equals(passwordFromDB,userPassword)) {
-                        loginUsername.setError(null);
-                        Intent intent = new Intent(login_Activity.this, selectionActivity.class);
-                        startActivity(intent);
-                    } else {
-                        loginPassword.setError("Invalid Password");
-                        loginPassword.requestFocus();
+                    for (DataSnapshot userSnapshot : snapshot.getChildren()) {
+                        String passwordFromDB = userSnapshot.child("pass").getValue(String.class);
+                        if (Objects.equals(passwordFromDB, userPassword)) {
+                            loginUsername.setError(null);
+                            saveLoggedStatus(true); // Mark user as logged in
+                            startSelectionActivity();
+                            return;
+                        }
                     }
+                    loginPassword.setError("Invalid Password");
+                    loginPassword.requestFocus();
                 } else {
-                    loginUsername.setError("Does not exist");
+                    loginUsername.setError("User does not exist");
                     loginUsername.requestFocus();
                 }
             }
+
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
+                // Handle the error if needed
             }
         });
+    }
+
+    private void saveLoggedStatus(boolean hasLogged) {
+        SharedPreferences sharedPreferences = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean(PREF_KEY_HAS_LOGGED, hasLogged);
+        editor.apply();
+    }
+
+    private void startSelectionActivity() {
+        Intent intent = new Intent(login_Activity.this, selectionActivity.class);
+        startActivity(intent);
+        finish(); // Close the login
     }
 }
