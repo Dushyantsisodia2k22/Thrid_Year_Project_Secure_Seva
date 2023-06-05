45b8c9d2573882d77069b7bb217e55473c29a84e
Dushyantsisodia2k22 <sisodiadushyant2002@gmail.com>
2023-06-05 22:56:14 +0530

    logout button working (no dialog box appear)

--- app/src/main/java/com/example/gla_final_2/selectionActivity.java
+++ app/src/main/java/com/example/gla_final_2/selectionActivity.java
@@ -1,14 +1,45 @@
 package com.example.gla_final_2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class selectionActivity extends AppCompatActivity {
+
+    public static final String PREFS_NAME = "MyPrefsFile";
+    public static final String PREF_KEY_HAS_LOGGED = "hasLogged";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selection);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ImageView logoutButton = findViewById(R.id.logoutButton);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle logout button click here
+                saveLoggedStatus(false); // Mark user as logged out
+
+                Intent intent = new Intent(selectionActivity.this, login_Activity.class);
+                startActivity(intent);
+                finish(); // Optionally finish the current activity
+            }
+        });
+    }
+
+    private void saveLoggedStatus(boolean hasLogged) {
+        SharedPreferences sharedPreferences = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean(PREF_KEY_HAS_LOGGED, hasLogged);
+        editor.apply();
     }
 }
