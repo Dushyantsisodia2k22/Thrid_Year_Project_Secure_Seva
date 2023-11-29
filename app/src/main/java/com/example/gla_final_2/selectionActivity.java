40cf28310d73be78cf0e23ced1462ba9bad0a9c5
Dushyantsisodia2k22 <sisodiadushyant2002@gmail.com>
2023-11-29 20:56:16 +0530

    add contact not working.

--- app/src/main/java/com/example/gla_final_2/selectionActivity.java
+++ app/src/main/java/com/example/gla_final_2/selectionActivity.java
@@ -1,45 +1,41 @@
 package com.example.gla_final_2;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.Spinner;
-import android.widget.Toast;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
-import java.util.ArrayList;
-import java.util.List;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
-public class selectionActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+import com.google.android.gms.maps.model.Marker;
+
+
+
+public class selectionActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     public static final String PREFS_NAME = "MyPrefsFile";
     public static final String PREF_KEY_HAS_LOGGED = "hasLogged";
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+    private static final String ACCESS_FINE_LOCATION = android.Manifest.permission.ACCESS_FINE_LOCATION;
 
-    private Spinner mainSpinner;
-    private Spinner nestedSpinner;
-    private Button searchButton;
-
-    private DatabaseReference busDataRef;
+    private GoogleMap mMap;
+    private Marker userMarker;
+    private boolean hasLogged = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selection);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         ImageView logoutButton = findViewById(R.id.logoutButton);
         logoutButton.setOnClickListener(new View.OnClickListener() {
@@ -54,85 +50,18 @@ public class selectionActivity extends AppCompatActivity {
             }
         });
 
-        mainSpinner = findViewById(R.id.firstSpinner);
-        nestedSpinner = findViewById(R.id.secondSpinner);
-        searchButton = findViewById(R.id.nextButton);
-
-        busDataRef = FirebaseDatabase.getInstance().getReference("Bus_Data");
-
-        initializeMainSpinner();
-
-        searchButton.setOnClickListener(new View.OnClickListener() {
+        ImageView contactButton = findViewById(R.id.contact);
+        contactButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String selectedMainItem = mainSpinner.getSelectedItem().toString();
-                String selectedNestedItem = nestedSpinner.getSelectedItem().toString();
-
-                Intent intent = new Intent(selectionActivity.this, MapsActivity.class);
-                intent.putExtra("selectedMainItem", selectedMainItem);
-                intent.putExtra("selectedNestedItem", selectedNestedItem);
+                // Handle contact button click here
+                Intent intent = new Intent(selectionActivity.this, AddContactActivity.class);
                 startActivity(intent);
             }
         });
-    }
 
-    private void initializeMainSpinner() {
-        busDataRef.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                final List<String> mainSpinnerItems = new ArrayList<>();
-
-                for (DataSnapshot childSnapshot : dataSnapshot.getChildren()) {
-                    String mainItem = childSnapshot.getKey();
-                    mainSpinnerItems.add(mainItem);
-                }
-
-                ArrayAdapter<String> mainSpinnerAdapter = new ArrayAdapter<>(selectionActivity.this, android.R.layout.simple_spinner_item, mainSpinnerItems);
-                mainSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                mainSpinner.setAdapter(mainSpinnerAdapter);
-
-                mainSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-                    @Override
-                    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                        String selectedItem = mainSpinnerItems.get(position);
-
-                        retrieveNestedSpinnerItems(selectedItem);
-                    }
-
-                    @Override
-                    public void onNothingSelected(AdapterView<?> parent) {
-                    }
-                });
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-                // Handle database read error if needed
-            }
-        });
-    }
-
-    private void retrieveNestedSpinnerItems(String selectedItem) {
-        busDataRef.child(selectedItem).addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                final List<String> nestedSpinnerItems = new ArrayList<>();
-
-                for (DataSnapshot childSnapshot : dataSnapshot.getChildren()) {
-                    String nestedItem = childSnapshot.getKey();
-                    nestedSpinnerItems.add(nestedItem);
-                }
-
-                ArrayAdapter<String> nestedSpinnerAdapter = new ArrayAdapter<>(selectionActivity.this, android.R.layout.simple_spinner_item, nestedSpinnerItems);
-                nestedSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                nestedSpinner.setAdapter(nestedSpinnerAdapter);
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-                // Handle database read error if needed
-            }
-        });
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
 
     private void saveLoggedStatus(boolean hasLogged) {
@@ -140,5 +69,31 @@ public class selectionActivity extends AppCompatActivity {
         SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.putBoolean(PREF_KEY_HAS_LOGGED, hasLogged);
         editor.apply();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (ContextCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            // Request location updates when the activity resumes
+            requestLocationUpdates();
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    private void requestLocationUpdates() {
+        // Implement code to request location updates here
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        if (ContextCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
+        }
     }
 }
