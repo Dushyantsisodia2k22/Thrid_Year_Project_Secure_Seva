4d07aded3e07a0ba915aaeecce86e76578743f1c
Dushyantsisodia2k22 <sisodiadushyant2002@gmail.com>
2023-12-03 22:35:08 +0530

    Sms working

--- app/src/main/java/com/example/gla_final_2/selectionActivity.java
+++ app/src/main/java/com/example/gla_final_2/selectionActivity.java
@@ -1,41 +1,64 @@
 package com.example.gla_final_2;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.location.Location;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
-
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-
 import com.google.android.gms.maps.model.Marker;
-
-
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class selectionActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     public static final String PREFS_NAME = "MyPrefsFile";
     public static final String PREF_KEY_HAS_LOGGED = "hasLogged";
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
-    private static final String ACCESS_FINE_LOCATION = android.Manifest.permission.ACCESS_FINE_LOCATION;
+    private static final int SMS_PERMISSION_REQUEST_CODE = 2;
+    private static final String ACCESS_FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
+    private static final String SEND_SMS = Manifest.permission.SEND_SMS;
 
     private GoogleMap mMap;
     private Marker userMarker;
     private boolean hasLogged = true;
+    private FusedLocationProviderClient fusedLocationClient;
+    private LocationCallback locationCallback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selection);
+
+        // Initialize FusedLocationProviderClient
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
         ImageView logoutButton = findViewById(R.id.logoutButton);
         logoutButton.setOnClickListener(new View.OnClickListener() {
@@ -50,13 +73,17 @@ public class selectionActivity extends AppCompatActivity implements OnMapReadyCa
             }
         });
 
-        ImageView contactButton = findViewById(R.id.contact);
-        contactButton.setOnClickListener(new View.OnClickListener() {
+        Button addconbtn = findViewById(R.id.addconbtn);
+        addconbtn.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
             @Override
             public void onClick(View v) {
-                // Handle contact button click here
-                Intent intent = new Intent(selectionActivity.this, AddContactActivity.class);
-                startActivity(intent);
+                // Check and request permissions for sending SMS
+                if (checkPermission(SEND_SMS)) {
+                    retrieveAndProcessPhoneNumbers();
+                } else {
+                    requestPermissions(new String[]{SEND_SMS}, SMS_PERMISSION_REQUEST_CODE);
+                }
             }
         });
 
@@ -71,26 +98,188 @@ public class selectionActivity extends AppCompatActivity implements OnMapReadyCa
         editor.apply();
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if (ContextCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            // Request location updates when the activity resumes
-            requestLocationUpdates();
+    private void retrieveAndProcessPhoneNumbers() {
+        if (checkPermission(ACCESS_FINE_LOCATION)) {
+            fusedLocationClient.getLastLocation().addOnSuccessListener(this, location -> {
+                if (location != null) {
+                    retrieveAndProcessPhoneNumbers(location);
+                } else {
+                    Toast.makeText(this, "Location not available", Toast.LENGTH_SHORT).show();
+                }
+            });
         } else {
             ActivityCompat.requestPermissions(this, new String[]{ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
         }
     }
 
-    private void requestLocationUpdates() {
-        // Implement code to request location updates here
+    private void retrieveAndProcessPhoneNumbers(Location location) {
+        DatabaseReference phoneNumbersRef = FirebaseDatabase.getInstance().getReference("phone_number");
+
+        Query orderedQuery = phoneNumbersRef.orderByKey();
+
+        orderedQuery.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    String key = snapshot.getKey();
+                    String phoneNumber = snapshot.getValue(String.class);
+
+                    // Check if the phone number is valid
+                    if (isValidPhoneNumber(phoneNumber)) {
+                        sendSMS(phoneNumber, location); // Pass the location to sendSMS
+                    } else {
+                        Toast.makeText(selectionActivity.this, "Invalid phone number: " + phoneNumber, Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Handle error
+                Toast.makeText(selectionActivity.this, "Error retrieving phone numbers", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private boolean isValidPhoneNumber(String phoneNumber) {
+        // Implement validation logic if needed
+        // For simplicity, this example considers all phone numbers as valid
+        return true;
+    }
+
+    private void sendSMS(String phoneNumber, Location location) {
+        try {
+            // Get the default instance of SmsManager
+            SmsManager smsManager = SmsManager.getDefault();
+
+            // Create the SMS message
+            String message = "Please help check my location: " + getGoogleMapsLink(location);
+
+            // Use SmsManager to send the message
+            smsManager.sendTextMessage(phoneNumber, null, message, null, null);
+
+            // Log SMS details
+            Log.d("SMSIntent", "Phone Number: " + phoneNumber + ", SMS Body: " + message);
+
+            // Display a success message or perform any other necessary actions
+            Toast.makeText(this, "Location Shared Successfully", Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            e.printStackTrace();
+            // Log or display an error message
+            Toast.makeText(this, "Error in sending Location", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private String getGoogleMapsLink(Location location) {
+        if (location != null) {
+            double latitude = location.getLatitude();
+            double longitude = location.getLongitude();
+            return "https://maps.google.com/?q=" + latitude + "," + longitude;
+        } else {
+            // Handle the case where location is null
+            return "";  // or provide a default link
+        }
+    }
+
+    private boolean checkPermission(String permission) {
+        return ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == SMS_PERMISSION_REQUEST_CODE) {
+            // Check if the SMS permission is granted
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Request location updates after SMS permission is granted
+                retrieveAndProcessPhoneNumbers();
+            } else {
+                // Handle the case where SMS permission is denied
+                Toast.makeText(this, "SMS permission denied", Toast.LENGTH_SHORT).show();
+            }
+        } else if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            // Handle location permission result
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission granted, start location updates or other tasks
+                startLocationUpdates();
+            } else {
+                // Handle the case where location permission is denied
+                Toast.makeText(this, "Location permission denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (checkPermission(ACCESS_FINE_LOCATION)) {
+            // Request location updates when the activity resumes
+            startLocationUpdates();
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    private void startLocationUpdates() {
+        // Location request settings
+        LocationRequest locationRequest = new LocationRequest();
+        locationRequest.setInterval(5000); // 5 seconds interval
+        locationRequest.setFastestInterval(3000); // 3 seconds fastest interval
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        // Location callback
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+                if (locationResult != null) {
+                    Location location = locationResult.getLastLocation();
+                    if (location != null) {
+                        // Update the user's location on the map or perform other location-related tasks
+                        updateLocationOnMap(location);
+                    }
+                }
+            }
+        };
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, null);
+    }
+
+    private void updateLocationOnMap(Location location) {
+        // Implement logic to update the user's location on the map
+        // For now, log the location details
+        Log.d("LocationUpdate", "Latitude: " + location.getLatitude() + ", Longitude: " + location.getLongitude());
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Stop location updates when the activity is paused
+        stopLocationUpdates();
+    }
+
+    private void stopLocationUpdates() {
+        if (locationCallback != null) {
+            fusedLocationClient.removeLocationUpdates(locationCallback);
+        }
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        if (ContextCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+        if (checkPermission(ACCESS_FINE_LOCATION)) {
             mMap.setMyLocationEnabled(true);
         } else {
             ActivityCompat.requestPermissions(this, new String[]{ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
